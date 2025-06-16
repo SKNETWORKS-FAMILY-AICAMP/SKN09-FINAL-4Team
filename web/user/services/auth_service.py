b1401b13c769b7084e8740f87a0ce2daf9110ebb
doyeon158 <183298259+doyeon158@users.noreply.github.com>
2025-06-16 17:13:35 +0900

    docker verson

--- web/user/services/auth_service.py
+++
@@ -1,7 +0,0 @@
-from ..repositories.user_repository import get_user_by_email
-
-def authenticate_user(email, password):
-    user = get_user_by_email(email)
-    if user and user.password == password:
-        return user
-    return None
